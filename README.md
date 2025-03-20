a74f232d1e971a62eba2a49b72c6a878f41ae9bb
allentown <tangxiangpi@gmail.com>
2025-03-20 19:45:04 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,122 @@
-# keyrix-example-stripe-integration-nodejs
-An example Node.js app that integrates Keyrix with Stripe for accepting payments.
+# Example Keyrix + Stripe integration
+The following web app is written in Node.js and shows how to integrate
+[Keyrix](https://keyrix.focusapps.app) and [Stripe](https://stripe.com) together
+using webhooks. Much more could be done to automate e.g. license
+revocation when a subscription is canceled, etc.
+
+> **This example application is not 100% production-ready**, but it should
+> get you 90% of the way there. You may need to add additional logging,
+> error handling, as well as listening for additional webhook events.
+>
+> If you are looking for next.js example, just implement the `/keyrix-webhooks` api route in your app.
+
+## Running the app
+
+First up, configure a few environment variables or rename the `.env.example` to `.env`:
+```bash
+# Stripe publishable key
+export STRIPE_PUBLISHABLE_KEY="YOUR_STRIPE_PUBLISHABLE_KEY"
+
+# Stripe secret key (don't share this!)
+export STRIPE_SECRET_KEY="YOUR_STRIPE_SECRET_KEY"
+
+# The Stripe plan to subscribe new customers to
+export STRIPE_PRICE_ID="YOUR_STRIPE_PRICE_ID"
+
+# Keyrix product token (don't share this!)
+export KEYRIX_PRODUCT_TOKEN="YOUR_KEYRIX_PRODUCT_TOKEN"
+
+# Your Keyrix account ID
+export KEYRIX_ACCOUNT_ID="YOUR_KEYRIX_ACCOUNT_ID"
+
+# The Keyrix policy to use when creating licenses for new users
+# after they successfully subscribe to a plan
+export KEYRIX_POLICY_ID="YOUR_KEYRIX_POLICY_ID"
+```
+
+You can either run each line above within your terminal session before
+starting the app, or you can add the above contents to your `~/.bashrc`
+file and then run `source ~/.bashrc` after saving the file.
+
+Next, install dependencies with [`yarn`](https://yarnpkg.comg):
+```
+yarn
+```
+
+Then start the app:
+```
+yarn start
+```
+
+## Configuring the webhooks
+
+For local development
+
+- ngrok
+create an [`ngrok`](https://ngrok.com) tunnel to your
+local development server:
+
+```bash
+ngrok http 8080
+```
+
+Next up, add the generated `ngrok` URL to your Stripe and Keyrix accounts to
+listen for webhooks.
+
+1. **Stripe:** add `https://{YOUR_NGROK_URL}/stripe-webhooks` to https://dashboard.stripe.com/account/webhooks
+
+you should add these event types depending on whether one-time or recurring:
+
+- `checkout.session.completed`
+- `charge.refunded`
+- `customer.created`
+- `customer.subscription.created`
+- `customer.subscription.updated`
+- `customer.subscription.deleted`
+
+1. **Keyrix:** add `https://{YOUR_NGROK_URL}/keyrix-webhooks` to https://keyrix-admin.focusapps.app/webhook-endpoints
+
+you should add these event types:
+
+- `user.created`
+
+> **In a production environment, you would use your actual server info in place of
+> the `ngrok` URLs above.**
+
+## Testing the integration
+
+Visit the following url: http://localhost:8080 and fill out the purchase form.
+
+## Common Issues
+
+### Incorrect ENV variables
+
+In case of errors, please double check all of your environment variables.
+If one of the variables are incorrect, it may cause API authentication
+issues.
+
+### Protected account
+
+**Please note that this example requires that your Keyrix account is
+set to unprotected**, because this example handles user creation
+on the front-end. You can update this setting on your [account's
+settings page](https://keyrix-admin.focusapps.app/settings). If you would prefer
+to keep your account protected, the logic for user creation would
+need to be moved to a server-side URL.
+
+### Other issues
+
+Here's a few things to double check when a problem arises:
+
+1. Make sure you're using the correct account ID (find yours [here](https://keyrix-admin.focusapps.app/settings))
+1. Make sure you're using a product token or admin token (the token should start with `prod-` or `admi-`)
+1. Make sure you're using the correct policy ID (it should be a UUID)
+1. Make sure that your Stripe environment variables are correct
+1. Make sure all dependencies have been installed via `yarn install`
+1. Make sure you have correctly configured webhooks for both Keyrix _and_ Stripe
+1. Make sure that the webhook URL is accessible from the public internet via `ngrok`
+
+## Questions?
+
+Reach out at [keyrix@focusapps.app](mailto:keyrix@focusapps.app) if you have any
+questions or concerns!
---
+++ index.js
@@ -0,0 +1,195 @@
+require('dotenv').config()
+// Be sure to add these ENV variables!
+const {
+  STRIPE_PUBLISHABLE_KEY,
+  STRIPE_SECRET_KEY,
+  STRIPE_PRICE_ID,
+  KEYRIX_PRODUCT_TOKEN,
+  KEYRIX_ACCOUNT_ID,
+  KEYRIX_POLICY_ID,
+  PORT = 8080
+} = process.env
+
+const stripe = require("stripe")(STRIPE_SECRET_KEY)
+const fetch = require("node-fetch")
+const express = require("express")
+const bodyParser = require("body-parser")
+const morgan = require('morgan')
+const app = express()
+
+app.use(bodyParser.json({ type: "application/vnd.api+json" }))
+app.use(bodyParser.json({ type: "application/json" }))
+app.use(morgan('combined'))
+
+app.set('view engine', 'ejs')
+
+app.post("/keyrix-webhooks", async (req, res) => {
+  const { data: { id: keyrixEventId } } = req.body
+
+  // Fetch the webhook to validate it and get its most up-to-date state
+  const keyrixWebhook = await fetch(`https://keyrix-api.focusapps.app/v1/accounts/${KEYRIX_ACCOUNT_ID}/webhook-events/${keyrixEventId}`, {
+    method: "GET",
+    headers: {
+      "Authorization": `Bearer ${KEYRIX_PRODUCT_TOKEN}`,
+      "Accept": "application/vnd.api+json"
+    }
+  })
+
+  const { data: keyrixEvent, errors } = await keyrixWebhook.json()
+  if (errors) {
+    return res.sendStatus(200) // Event does not exist (wasn't sent from Keyrix)
+  }
+
+  switch (keyrixEvent.attributes.event) {
+    // 1. Respond to user creation events within your Keyrix account. Here, we'll create
+    //    a new Stripe customer account for new Keyrix users.
+    case "user.created":
+      const { data: keyrixUser } = JSON.parse(keyrixEvent.attributes.payload)
+
+      // Make sure our Keyrix user has a Stripe token, or else we can't charge them later on..
+      if (!keyrixUser.attributes.metadata.stripeToken) {
+        throw new Error(`User ${keyrixUser.id} does not have a Stripe token attached to their user account!`)
+      }
+
+      // 2. Create a Stripe customer, making sure we use our Stripe token as their payment
+      //    method of choice.
+      const stripeCustomer = await stripe.customers.create({
+        description: `Customer for Keyrix user ${keyrixUser.attributes.email}`,
+        email: keyrixUser.attributes.email,
+        // Source is a Stripe token obtained with Stripe.js during user creation and
+        // temporarily stored in the user's metadata attribute.
+        source: keyrixUser.attributes.metadata.stripeToken,
+        // Store the user's Keyrix ID within the Stripe customer so that we can lookup
+        // a Stripe customer's Keyrix account.
+        metadata: { keyrixUserId: keyrixUser.id }
+      })
+
+      // 3. Add the user's Stripe customer ID to the user's metadata attribute so that
+      //    we can lookup their Stripe customer account when needed.
+      const update = await fetch(`https://keyrix-api.focusapps.app/v1/accounts/${KEYRIX_ACCOUNT_ID}/users/${keyrixUser.id}`, {
+        method: "PATCH",
+        headers: {
+          "Authorization": `Bearer ${KEYRIX_PRODUCT_TOKEN}`,
+          "Content-Type": "application/vnd.api+json",
+          "Accept": "application/vnd.api+json"
+        },
+        body: JSON.stringify({
+          data: {
+            type: "users",
+            attributes: {
+              metadata: { stripeCustomerId: stripeCustomer.id }
+            }
+          }
+        })
+      })
+
+      const { data, errors } = await update.json()
+      if (errors) {
+        throw new Error(errors.map(e => e.detail).toString())
+      }
+
+      // All is good! Stripe customer was successfully created for the new Keyrix
+      // user. Let Keyrix know the event was received successfully.
+      res.sendStatus(200)
+      break
+    default:
+      // For events we don't care about, let Keyrix know all is good.
+      res.sendStatus(200)
+  }
+})
+
+app.post("/stripe-webhooks", async (req, res) => {
+  const { body: stripeEvent } = req
+
+  switch (stripeEvent.type) {
+    // 4. Respond to customer creation events within your Stripe account. Here, we'll
+    //    create a new Stripe subscription for the customer as well as a Keyrix license
+    //    for the Keyrix user that belongs to the Stripe customer.
+    case "customer.created":
+      const { object: stripeCustomer } = stripeEvent.data
+
+      // Make sure our Stripe customer has a Keyrix user ID, or else we can't work with it.
+      if (!stripeCustomer.metadata.keyrixUserId) {
+        throw new Error(`Customer ${stripeCustomer.id} does not have a Keyrix user ID attached to their customer account!`)
+      }
+
+      // 5. Create a subscription for the new Stripe customer. This will charge the
+      //    Stripe customer. (You may or may not want to also check if the customer
+      //    already has an existing subscription.)
+      const stripeSubscription = await stripe.subscriptions.create({
+        customer: stripeCustomer.id,
+        plan: STRIPE_PRICE_ID
+      }, {
+        // Use an idempotency key so that we don't charge a customer more than one
+        // time regardless of how many times this webhook is retried.
+        // See: https://stripe.com/docs/api/node#idempotent_requests
+        idempotency_key: stripeCustomer.metadata.keyrixUserId
+      })
+
+      // 6. Create a license for the new Stripe customer after we create a subscription
+      //    for them. We're pulling the Keyrix user's ID from the Stripe customer's
+      //    metadata attribute (we stored it there earler).
+      const keyrixLicense = await fetch(`https://keyrix-api.focusapps.app/v1/accounts/${KEYRIX_ACCOUNT_ID}/licenses`, {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${KEYRIX_PRODUCT_TOKEN}`,
+          "Content-Type": "application/vnd.api+json",
+          "Accept": "application/vnd.api+json"
+        },
+        body: JSON.stringify({
+          data: {
+            type: "licenses",
+            attributes: {
+              metadata: { stripeSubscriptionId: stripeSubscription.id }
+            },
+            relationships: {
+              policy: {
+                data: { type: "policies", id: KEYRIX_POLICY_ID }
+              },
+              user: {
+                data: { type: "users", id: stripeCustomer.metadata.keyrixUserId }
+              }
+            }
+          }
+        })
+      })
+
+      const { data, errors } = await keyrixLicense.json()
+      if (errors) {
+        res.sendStatus(500)
+
+        // If you receive an error here, then you may want to handle the fact the customer
+        // may have been charged for a license that they didn't receive e.g. easiest way
+        // would be to create it manually, or refund their subscription charge.
+        throw new Error(errors.map(e => e.detail).toString())
+      }
+
+      // All is good! License was successfully created for the new Stripe customer's
+      // Keyrix user account. Next up would be for us to email the license key to
+      // our user's email using `stripeCustomer.email` or something similar.
+
+      // Let Stripe know the event was received successfully.
+      res.sendStatus(200)
+      break
+    default:
+      // For events we don't care about, let Stripe know all is good.
+      res.sendStatus(200)
+  }
+})
+
+app.get('/', async (req, res) => {
+  res.render('index', {
+    STRIPE_PUBLISHABLE_KEY,
+    KEYRIX_ACCOUNT_ID
+  })
+})
+
+process.on('unhandledRejection', err => {
+  console.error(`Unhandled rejection: ${err}`, err.stack)
+})
+
+const server = app.listen(PORT, 'localhost', () => {
+  const { address, port } = server.address()
+
+  console.log(`Listening at http://${address}:${port}`)
+})
